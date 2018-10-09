a6868110653b1a417dbb34ec217793215912eb00
Ian Bovard <thebestpantsintown@gmail.com>
2018-10-09 11:43:39 -0500

    resized and restyled page, added unselected credit card logos

--- src/ATM/atm_cards.js
+++ src/ATM/atm_cards.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
-import cardLogos from './assets/creditcard_sprite.png';
+import starLogo from './assets/star_logo.png';
+import pulseLogo from './assets/pulse_logo.png';
+import maestroLogo from './assets/maestro_logo.png';
+import mastercardLogo from './assets/mastercard_logo.png';
+import plusLogo from './assets/plus_logo.png';
+import visaLogo from './assets/visa_logo.png';
 
 class AtmCards extends Component {
 	render() {
 		return(
-			<div>
-				<img src={cardLogos} className="card-logos" alt="logos"/>
+			<div className="card-logos">
+				<img src={starLogo} className="star-logo" alt="star"/>
+				<img src={pulseLogo} className="pulse-logo" alt="pulse"/>
+				<img src={maestroLogo} className="maestro-logo" alt="maestro"/>
+				<img src={mastercardLogo} className="mastercard-logo" alt="mastercard"/>
+				<img src={plusLogo} className="plus-logo" alt="plus"/>
+				<img src={visaLogo} className="visa-logo" alt="visa"/>
 			</div>
 			)
 	}
