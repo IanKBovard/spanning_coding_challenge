ae871ba14ee53c1518bc19036d77f620779ccd9d
Ian Bovard <thebestpantsintown@gmail.com>
2018-10-08 23:32:28 -0500

    added button text

--- src/ATM/atm_screen_text.js
+++ src/ATM/atm_screen_text.js
@@ -4,16 +4,20 @@ class AtmScreenText extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			withdrawInput : 'Withdraw',
-			depositInput: 'Deposit',
-			balanceInput: 'Balance',
-			reenterPinInput: 'Re-Enter PIN'
+			withdrawText : 'Withdraw',
+			depositText: 'Deposit',
+			balanceText: 'Balance',
+			reenterPinText: 'Re-Enter PIN'
 		}
 	}
 	render() {
 		return(
 			<div className="text-container">
 				<span className={this.props.screenTextClass}>{this.props.screenText}</span>
+				<span id="withdraw-button-text"class="button-text">{this.state.withdrawText}</span>
+				<span id="deposit-button-text"class="button-text">{this.state.depositText}</span>
+				<span id="balance-button-text"class="button-text">{this.state.balanceText}</span>
+				<span id="reenter-pin-button-text"class="button-text">{this.state.reenterPinText}</span>
 				<div id="withdraw-text-deco" class="text-deco"></div>
 				<div id="balance-text-deco" class="text-deco"></div>
 				<div id="deposit-text-deco" class="text-deco"></div>
